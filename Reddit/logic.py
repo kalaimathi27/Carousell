9b603750f7ed03926daef1d466b0f804746b831f
kkuser <kkuser@192.168.1.11>
2017-04-09 16:15:36 +0800

    committing initial trial steps to add topics

---
+++ Reddit/logic.py
@@ -0,0 +1,50 @@
+'''
+Created on Apr 9, 2017
+
+@author: kkuser
+'''
+import datetime
+
+def get_initial_topics():
+    try:
+        text_list = ["Music is all to me",
+                     "NASA unveiled new plans for getting humans to Mars, and hardly anyone noticed",
+                     "20 years ago today, Third Eye Blind released their self-titled album including Jumper and Semi-Charmed Life",
+                     "U.S. Navy strike group to move toward Korean peninsula: U.S. official",
+                     "CItizens of Krakow have built a hive for 1500 bees in the middle of the city",
+                     "'Sons of Guns' star Will Hayden found guilty in sexual assaults of 2 girls; sentenced to life in prison"]
+        topic_type = ["Music", "Space", "Music", "Army", "Public", "Movie"]
+        topics = []
+        temp_topic = Topic("Temp topic", "", "", "admin", "trending")
+        
+        for i in range(5):
+            temp_topic.upvotes = 200+i*10
+            temp_topic.downvotes = 100-i*2
+            temp_topic.title = text_list[i]
+            temp_topic.topic_type = topic_type[i]
+            topics.append(temp_topic.get_topic())
+        return topics
+    except Exception as exce:
+        raise exce
+
+class Topic:
+    """
+    Class to initialize a topic
+    """
+    def __init__(self, title, text, sub_reddit, user_created, topic_type):
+        self.title = title
+        self.text = text
+        self.sub_reddit = sub_reddit
+        self.upvotes = 0
+        self.downvotes = 0
+        self.user_created = user_created
+        self.topic_type = topic_type
+        self.created_date = datetime.datetime.now()
+    
+    def get_topic(self):
+        return {"title": self.title,
+                "text": self.text,
+                "sub_redit": self.sub_reddit,
+                "upvotes": self.upvotes,
+                "downvotes": self.downvotes}
+        
--- Reddit/views.py
+++ Reddit/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
+from Reddit.logic import get_initial_topics
+
+
 
 def index(request):
+	
+	initial_topics = get_initial_topics()
+	context = {"context" : initial_topics}
 	template = loader.get_template('Reddit/index.html')
-	return HttpResponse(template.render({}, request))
+	return HttpResponse(template.render(context, request))
