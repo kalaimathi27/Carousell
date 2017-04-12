c8a426a92a9a5ac03c66ff2c3e5847a2b4163206
kkuser <kkuser@192.168.1.11>
2017-04-12 20:38:00 +0800

    populating more topics

--- Reddit/logic.py
+++ Reddit/logic.py
@@ -23,14 +23,27 @@ def get_initial_topics():
                      "United CEO doubles down in email to employees, says passenger was 'disruptive and belligerent'",
                      "In 2012, United Airlines Employees Called Disabled Vet 'retard' & Abused His Service Dog",
                      "Trump voters will talk about Hillary even when Trump ends up killing us all.",
-                     "United Airlines stock down over 5% premarket trading"]
+                     "United Airlines stock down over 5% premarket trading",
+                     "On this date in 1961, Yuri Gagarin became the first human to journey into outer space. Happy Yuri's Night!",
+                     "Doctor does all he can to get to patients before being forced off United flight",
+                     "Tennessee Could Give Taxpayers America's Fastest Internet For Free, But It Will Give Comcast and AT&T $45 Million Instead",
+                     "Stephen Colbert throws staff pizza parties when he beats Jimmy Fallon's ratings.",
+                     "The Fast and the Furious movies that feature Corona being consumed average $250 million domestically and have a 63% combined IMDb/RT score. The Fast movies that don't feature Corona average $163 million domestically and have a 57% RT/IMDb average.",
+                     "Don't donate to wildlife charities, instead just hand the money straight to the animals so that the business doesn't get a cut of the cash",
+                     "Donald Trump proposes marriage to Melania, 2004",
+                     "Does pupil constriction only happen when your eye is exposed to light in the visible spectrum?",
+                     "Gif used by Wikipedia to explain how cats always land on their feet",
+                     "New research suggests California's weather is defying the odds. Meteorologists expected precipitation debts accrued during California's historic drought to last decades, but a new analysis suggests the debts could be erased this year.",
+                     ]
         
         topic_type = ["Music", "Space", "Music", "Army", "Public", "Movie", "World News", 
-                      "World News", "News", "Politics", "Business"]
+                      "World News", "News", "Politics", "Business", "Facts", "News", "World",
+                      "Entertainment", "Entertainment", "Comedy", "History",
+                      "Question", "Random", "World"]
         topics = []
         
         
-        for i in range(11):
+        for i in range(21):
             temp_topic = Topic("Temp topic", "", "admin", "trending")
             temp_topic.upvotes = i
             temp_topic.downvotes = i-1
