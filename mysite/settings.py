4ae9cb6b22a74abffabb776af0c4e5e55da41e97
kkuser <kkuser@192.168.1.11>
2017-04-12 18:49:14 +0800

    committing venv changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
